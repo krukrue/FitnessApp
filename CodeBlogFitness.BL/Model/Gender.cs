14a7af30b845c7976d8acb48693d0213b1920f0c
krukrue <92872517+krukrue@users.noreply.github.com>
2022-11-15 15:47:50 +0100

    Created User Model

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; set; }
+        public Gender (string NAME)
+        {
+            if (NAME.ToUpper() != "M" || NAME.ToUpper() != "F"){
+                throw new ArgumentNullException("Gender can be M or F");
+            }
+            Name = NAME;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class User
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DateTime BirthDate { get; }
+        public double Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public User(string NAME, Gender GENDER, DateTime BIRTHDATE, double WEIGHT, int HEIGHT)
+        {
+            if (string.IsNullOrEmpty(NAME))
+            {
+                throw new ArgumentNullException("Name is incorrect");
+
+            }
+            if (GENDER == null)
+            {
+                throw new ArgumentNullException("Gender is incorrect");
+            }
+
+            if (BIRTHDATE < DateTime.Parse("01.01.1910") || BIRTHDATE > DateTime.Parse("15.11.2020"))
+            {
+                throw new ArgumentException("BirthDate is incorrect");
+            }
+
+            if (WEIGHT < 0)
+            {
+                throw new ArgumentException("Weight less than 0");
+
+            }
+
+            if (HEIGHT < 20)
+            {
+                throw new ArgumentException("Height less than 0");
+
+            }
+
+            Name = NAME;
+            Gender = GENDER;
+            Height = HEIGHT;
+            BirthDate = BIRTHDATE;
+            Weight = WEIGHT;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Gender.Name}";
+        }
+
+    }
+}
